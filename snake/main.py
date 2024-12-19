002dfc1936170f8c8110203d503bb2ab8327fc6b
arthurgauffre <arth.gauffre@gmail.com>
2024-12-19 17:57:55 +0100

    Add training logic, agent model, and visualization for snake game AI

--- snake/main.py
+++ snake/main.py
@@ -1,7 +1,49 @@
-import torch
-def main():
-    x = torch.rand(5, 3)
-    print(x)
+from src.agents.snake_agent import Agent
+from src.games.snake_game_ai import SnakeGameAI
+from src.agents.helper import plot
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                agent.model.save()
+
+            print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+            plot_scores.append(score)
+            total_score += score
+            mean_score = total_score / agent.n_games
+            plot_mean_scores.append(mean_score)
+            plot(plot_scores, plot_mean_scores)
+
 
 if __name__ == '__main__':
-    main()
+    train()
